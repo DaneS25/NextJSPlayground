d2818d80d99ab2557832be2c3c93e969d75cec98
DaneS25 <soredt1@student.op.ac.nz>
2022-09-30 10:31:23 +1300

    fun little mess around project beginnings

---
+++ playground/pages/index.js
@@ -0,0 +1,33 @@
+import Navbar from './components/navbar';
+import Image from 'next/image';
+
+export default function Home() {
+  return <div>
+      <Navbar/>
+      <div class="home-page">
+      <div class="gif">
+        <Image src="https://media0.giphy.com/media/3o7aD7qEvVKEdCoOtO/giphy.gif" height={300} width={300}/>
+      </div>
+        <div class="login-box">
+          <h2>Login</h2>
+          <form>
+            <div class='user-box'>
+              <input type="text" name="" required=""/>
+              <label>username</label>
+            </div>
+            <div class='user-box'>
+            <input type="password" name="" required=""/>
+              <label>password</label>
+            </div>
+            <div class='button-form'>
+              <a id="submit" href="#">submit</a>
+              <div id="register">
+                Don't have an account?
+                <a href='#'>register</a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+}
